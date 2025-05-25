e73fe47bcb9fc1ee06faaecf85afa6e87f0d9dad
Avinash <avimalhotra505@gmail.com>
2025-05-25 11:12:52 +0530

    Node JS Architecture , V8, Libuv, Eevent loop

--- src/app.js
+++ src/app.js
@@ -11,3 +11,25 @@
 // console.log( name );
 // console.log( pi );
 
+
+// console.log(1);
+// setTimeout(()=>console.log(2),0);
+// console.log(3);
+
+const fs=require('fs');
+
+// const res=fs.readFileSync("src/data.txt",{encoding:"utf-8"});
+
+ fs.readFile('src/data.txt',{encoding:'utf-8'},(err,data)=>{
+    //  if(err){ console.warn(err) }
+    //  else{ console.log(data) }
+
+    console.log("start");
+    setImmediate(()=>console.log("immediate"));
+    setTimeout(()=>console.log("timeout"));
+    Promise.resolve("promise").then(i=>console.log(i));
+    process.nextTick(()=>console.log("next tick"));
+    console.log("end");
+ 
+
+ });
